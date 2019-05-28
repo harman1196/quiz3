d96627ae087a400bad534e9eff9053ef69a74e41
747624 <747624@123-PC31.student.Tor.Loc>
2019-05-28 16:12:03 -0400

    first commit

--- src/quiz/main.java
+++ src/quiz/main.java
@@ -5,6 +5,26 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+	String a = "copyandpastecopyandpastecopyandpastecopyandpastecopyandpastecopyandpastecommitandpushcommitandpushcommitandpushcommitandpushpushcccccommmitttsss";
+
+	int numChar =a.length();
+    System.out.println("number of characters in string:  "+numChar);
+    
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
